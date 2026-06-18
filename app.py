4cc6a4c63867e70d609b535e6706e76a035141b9
gyamficodes <gyamfijohn08@gmail.com>
2026-06-18 11:03:46 +0100

    learing variable,  output  prints etc

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
+import sys 
+
+
+print(sys.version);
 print("Hello World");
 
 
+# Python Indentation
+# Python uses indentation to indicate a block of code.
+if 5 > 2:
+  print("Five is greater than two!");
+if 5 > 2:
+  print("Five is greater than two!");
 
 
+# Python Variables
+x = 10
+print(x)
 
 
+# Python Statements
+# A computer program is a list of "instructions" to be "executed" by a computer.
+# In a programming language, these programming instructions are called statements.
+# The following statement prints the text "Python is fun!" to the screen:
+
+print("Python is fun!")
+
+# Python Output / Print
+print("Hello World!")
+print('This will also work!')
+print("Hello World!", end=" gerdedrg\n")
 
 
-
-
+# Print Numbers
+print(111111)
+print(10 + 15)
+print("I am", 35, "years old.")
